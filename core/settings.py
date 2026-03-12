641bb404022fe7e5e4c80658bb8459310ca689c7
Bryan Eduardo Barba Estrada <u202323479@upc.edu.pe>
2026-03-11 23:57:52 -0500

    feat: add initial migrations for catalog, clients, orders, and payments apps; update secret key configuration

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f*u25=n)7o%o&c=#sk%nt7!h%i=(b3mavp3)#nk59u&f=iu2(@"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
