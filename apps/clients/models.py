e10e05e1f8d62731e09acc5a58bda864ab002a88
Bryan Eduardo Barba Estrada <u202323479@upc.edu.pe>
2026-03-12 10:24:45 -0500

    feat: update phone_number field

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Client(models.Model):
     name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=20, unique=True, blank=True, default="")
+    phone_number = models.CharField(max_length=20, unique=True, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
